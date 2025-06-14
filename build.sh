5b169a16d727dc41795f04924b066d3d71d7ed28
Abdullah Ibrahim Dar <ai0064125@gmail.com>
2025-06-14 09:14:05 +0530

    Add build script for Render deployment

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+# Exit immediately on error
+set -o errexit
+
+# Install Node.js (required for django-tailwind)
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install -y nodejs
+
+# Install Tailwind CSS dependencies
 npm install --prefix theme/static_src
 
-
+# Build Tailwind CSS
 python manage.py tailwind build
 
-
+# Run Django migrations
 python manage.py migrate
 
-
+# Collect static files
 python manage.py collectstatic --noinput

9d1affdac70635f6715203900c8654ec4b8d74e3
Abdullah Ibrahim Dar <ai0064125@gmail.com>
2025-06-14 09:33:06 +0530

    reverted

--- clone/settings.py
+++ clone/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/app/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
+STATIC_DIR = os.path.join(BASE_DIR, 'app/static')
 
 
 MEDIA_URL = '/images/'

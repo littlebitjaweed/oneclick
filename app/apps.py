b32fe119a7f65173ad3dfc402f903bf6ad910214
Abdullah Ibrahim Dar <ai0064125@gmail.com>
2025-06-14 10:59:11 +0530

    added json and apps.py

--- app/apps.py
+++ app/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
+from django.db.utils import OperationalError, ProgrammingError
+from django.core.management import call_command
 
 
 class AppConfig(AppConfig):
@@ -7,3 +9,11 @@ class AppConfig(AppConfig):
 
     def ready(self):
         import app.signals
+
+        try:
+            from django.contrib.auth.models import User
+            if not User.objects.exists():  # Only load if DB is empty
+                call_command('loaddata', 'fixtures/data.json')
+        except (OperationalError, ProgrammingError):
+            # Happens during first migration — ignore safely
+            pass
